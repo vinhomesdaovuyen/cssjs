05515f97b50caaeb38f688fd11d7326b4b83bd97
vinhomesdaovuyen <106257535+vinhomesdaovuyen@users.noreply.github.com>
2022-05-25 21:45:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://vinhomesdaovuyen.blogspot.com'>https://vinhomesdaovuyen.blogspot.com</a>";
+    var pagelink = "Muốn copy vui lòng liên hệ: 096 737 8889";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
